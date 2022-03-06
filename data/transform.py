8a142b01c865020f9307f54360b2ceb35fa4fed7
huypq <huyphamquoc@admicro.vn>
2022-03-06 10:26:30 +0700

    transform

--- data/transform.py
+++ data/transform.py
@@ -1,15 +1,38 @@
+from statistics import mode
+import torch.nn as nn
+from torch.nn import functional
 from torchvision import transforms as T
+
+class CropSize(nn.Module):
+    def __init__(self, size):
+        super(CropSize, self).__init__()
+        self.size = size
+    
+    def forward(self, image):
+        '''
+            image: tensor image
+        '''
+        h, w = image.shape[-2:]
+        delta_x = self.size - w
+        delta_y = self.size - h
+        left = int(delta_x / 2)
+        right = delta_x - left
+        top = int(delta_y / 2)
+        bottom = delta_y - top
+        return functional.pad(image, (left, right, top, bottom), mode='replicate')
+
+
+    def __repr__(self) -> str:
+        return f'Padding with width x height: {self.size} x {self.size}'
 
 to_tensor = T.Compose([
     T.ToTensor(),
-    T.Resize((64, 64)),
+    CropSize(64),
     T.Normalize(mean=[0.9, 0.9, 0.9], std=[0.1, 0.1, 0.1]),
 ])
 
-color = T.RandomApply(T.ColorJitter(0, 0, 0, 0), 0.2)
-
 augmenter = T.Compose([
-    color,
+    T.ColorJitter(0.2, 0.1, 0.5, 0.2),
     T.RandomGrayscale(0.2),
     to_tensor
 ])
