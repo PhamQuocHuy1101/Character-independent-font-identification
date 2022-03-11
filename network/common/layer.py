5ac3d42a036e2d4b3d895e4cb680fb9ec8d2a721
PhamQuocHuy1101 <phuy099@gmail.com>
2022-03-11 21:50:16 +0700

    dist

--- network/common/layer.py
+++ network/common/layer.py
@@ -2,10 +2,10 @@ import torch
 import torch.nn as nn
 
 class Conv2d(nn.Module):
-    def __init__(self, in_channel, out_channel, kernel, stride, padding = 'valid', act = True):
+    def __init__(self, in_channel, out_channel, kernel_size, stride, padding = 'valid', act = True):
         super().__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channel, out_channel, kernel, stride, padding),
+            nn.Conv2d(in_channel, out_channel, kernel_size, stride),
             nn.BatchNorm2d(out_channel)
         )
         self.act = nn.ReLU() if act == True else None
